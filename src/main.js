8e9e70e006e3c27cb9b533e55a8e6bd48124e8df
DarknessInsideU <51133202+Abdallah2358@users.noreply.github.com>
2022-07-14 15:31:25 +0200

    updated main, package, and router

--- src/main.js
+++ src/main.js
@@ -1,76 +1,28 @@
 import { createApp } from 'vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import App from './App.vue'
+import App from '@/App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-import { createRouter,createWebHistory } from 'vue-router'
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-import basebutton from './components/UI/basebutton.vue'
-import basewrapper from './components/UI/basewrapper.vue'
+import basebutton from '@/components/UI/basebutton.vue'
+import basewrapper from '@/components/UI/basewrapper.vue'
 
-import login from './components/pages/authentication/login.vue'
-import landingpage from './components/pages/unauthorized/landingpage.vue'
-
-import useraccount from './components/pages/user/account.vue'
-
-import connectus from './components/pages/unauthorized/connectus.vue'
-import donateform from './components/pages/unauthorized/donation-form.vue'
-import volunteerform from './components/pages/authentication/volunteer-form.vue'
-import signup from './components/pages/authentication/signup.vue'
-import forgetpass from './components/pages/authentication/forgetpassword.vue'
-import checkcode from './components/pages/authentication/checkcode.vue'
-import resetpass from './components/pages/authentication/resetpass.vue'
-import askforhelp from './components/pages/unauthorized/askforhelp.vue'
-import vagrants from './components/pages/unauthorized/vagrants.vue' 
-import helphomeless from './components/pages/unauthorized/helphomeless.vue' 
-
-import homelesschildren from './components/pages/unauthorized/homelesschildren.vue'
-import zakat from './components/pages/unauthorized/zakat.vue'
-import awarenesslectures from './components/pages/unauthorized/awarenesslectures.vue'
-import attendlecture from './components/pages/unauthorized/attendlecture.vue'
-import Jobs from './components/pages/unauthorized/Jobs.vue'
-import applyforjob from './components/pages/unauthorized/applyforjob.vue'
-import supportedservices from './components/pages/unauthorized/supportedservices.vue'
-import charitypage from './components/pages/unauthorized/charitypage.vue'
-import activitydetails from './components/pages/unauthorized/activitydetails.vue'
-
-import whoweare from './components/pages/unauthorized/whoweare.vue'
+import router from '@/router';
 
 const app = createApp(App)
-const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes: [
-        { path:'/',component:landingpage },
-        { path:'/login',component:login },
-        { path:'/myaccount',component:useraccount},
-        { path:'/connectus',component:connectus},
-        { path:'/whoweare',component:whoweare},
-        { path:'/donate-form/:charityid?',component:donateform},
 
-        { path:'/volunteer-form/:charityid?',component:volunteerform},
-        { path:'/signup',component:signup},
-        { path:'/forgetpass',component:forgetpass},
-        { path:'/checkcode/:email',component:checkcode},
-        { path:'/resetpass/:email',component:resetpass},
-        { path:'/askforhelp/:charityid?',component:askforhelp},
-        { path:'/vagrants',component:vagrants},
-        { path:'/helphomeless',component:helphomeless},
-        { path:'/homelesschildren',component:homelesschildren},
-        { path:'/zakat',component:zakat},
-        { path:'/awarenesslectures',component:awarenesslectures},
-        { path:'/attendlecture',component:attendlecture},
-        { path:'/Jobs',component:Jobs},
-        { path:'/applyforjob',component:applyforjob},
-        { path:'/supportedservices',component:supportedservices},
-        { path:'/charitypage/:charityid',component:charitypage},
-        { path:'/activitydetails/:activityid',component:activitydetails},
-    ] 
-})
+app.config.globalProperties.auth = false;
+app.config.globalProperties.api = 'http://localhost:8000/api';
 
 import { dom } from '@fortawesome/fontawesome-svg-core'
 dom.watch()
+import { createPinia } from 'pinia'
+app.use(createPinia())
+
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faPhone,faEnvelope,faLocationDot,faSearch , faPhoneVolume, faQuestion, faSignOutAlt, faBars, faCaretDown, faTimes, faHome, faSignInAlt, faUser, faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
@@ -80,6 +32,8 @@ library.add(faPhone,faEnvelope,faLocationDot,faSearch, faPhoneVolume, faQuestion
 
 app.component('fa', FontAwesomeIcon)
 app.use(router)
+app.use(VueAxios, axios)
+
 app.component('base-button',basebutton)
 app.component('base-wrapper',basewrapper)
 

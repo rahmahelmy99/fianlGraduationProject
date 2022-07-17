ec5b864304c826e879cce7a52b78f1fe2f7e616e
عبدالله الحديدي <51133202+Abdallah2358@users.noreply.github.com>
2022-07-18 00:10:28 +0200

    auth store done

--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -1,37 +1,63 @@
 import { defineStore } from 'pinia'
-// import axios from 'axios';
+
+const apiURL = 'http://localhost:8000/api'
 export const useAuthStore = defineStore('AuthStore', {
     state: () => {
-        return { token: '' }
+        return {
+            token: '',
+            isAuth: false,
+        }
     },
-
     actions: {
         async login(email, password) {
-            const response = await this.axios.post('/auth/token/',
+            const response = await fetch(`${apiURL}/auth/token/`,
                 {
-                    email: email,
-                    password: password
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(
+                        {
+                            email: email,
+                            password: password
+                        })
                 });
+            console.log("In login", response.data);
             this.token = response.data
+            this.isAuth = true
             localStorage.setItem('token', response.data);
-            router.push('/myaccount');
+            return response.data
         },
         async signup(data) {
             const { name, email, password, age, phone, address, nat_ID, blood } = data;
-            const reg = await this.axios.post('/users/create/',
+            const reg = await fetch(`${apiURL}/users/create/`,
                 {
-                    name: name,
-                    email: email,
-                    password: password,
-                    age: age,
-                    phone: phone,
-                    address: address,
-                    nat_ID: nat_ID,
-                    blood: blood,
-                });
-            this.login(email, password);
-            router.push('/myaccount');
-
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(
+                        {
+                            name: name,
+                            email: email,
+                            password: password,
+                            age: age,
+                            phone: phone,
+                            address: address,
+                            nat_ID: nat_ID,
+                            blood: blood,
+                        })
+                }).then(() => {
+                    const token = this.login(email, password);
+                    return token
+                })
+            console.log(reg);
+            return reg
         },
+        logout() {
+            this.token = ''
+            localStorage.removeItem('token');
+            this.isAuth = false;
+        }
     },
 })
